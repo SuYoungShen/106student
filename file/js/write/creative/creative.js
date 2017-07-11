cb3e8689b8ae5067aea2d1dd4f028d0a7bf6233a
unknown <k90218104@gcloud.csu.edu.tw>
2017-07-11 14:31:45 +0800

    all

---
+++ file/js/write/creative/creative.js
@@ -0,0 +1,70 @@
+﻿	
+	function CRanking(i){
+		var sc = "　";
+		var topics = [
+						"第一名",
+						"第二名",
+						"第三名",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作"
+						
+					];
+		CR = topics.length;		
+		return topics[i];
+	}
+	
+	function CTopic(i){
+		var topics = [
+						"虛與實",
+						"我的網路之旅",
+						"當網路成為習慣",
+						"網路面具",
+						"天涯若比鄰，人卻漸相離",
+						"致陪伴我成長的你",
+						"戴上面具的匿名舞會",
+						"網路心經",
+						"陳宜安"
+						
+						
+					];
+		
+		return topics[i];
+	}
+	
+	function CClasses(i){
+		var classess = [
+						"日間部四技建築一甲",
+						"日間部四技資管一乙",
+						"日間部四技建築一甲",
+						"日間部四技資工一乙",
+						"日間部四技金融一甲",
+						"日間部四技休運一乙",
+						"日間部四技建築一甲",
+						"日間部四技企管一甲",
+						"日間部四技國企二甲"
+						
+						];
+		return classess[i];
+	}
+	
+	function CNames(i){
+		var namess = [
+						"王咨淨",
+						"林郁宸",
+						"陳柔安",
+						"林廷翰",
+						"陳意庭",
+						"鍾漢昇",
+						"陳郁融",
+						"林昀諺",
+						"劉鴻慶"			
+						
+					];
+		return namess[i];
+	}
+	CRanking();
+	
---
+++ file/js/write/more/more.js
@@ -0,0 +1,54 @@
+﻿	
+	function MORanking(i){
+		var sc = "　";
+		var topics = [
+						"第一名",
+						"第二名",
+						"第三名",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作",
+						"佳"+sc+"作"
+						
+					];
+		MOR = topics.length;		
+		return topics[i];
+	}
+	
+	function MOClasses(i){
+		var classess = [
+						"日間部四技應外一甲",
+						"日間部四技應外一乙",
+						"日間部四技幼保一乙",
+						"日間部四技數位三甲",
+						"日間部四技資工一乙",
+						"日間部四技餐飲一甲",
+						"日間部四技數位延二甲",
+						"日間部四技應外一甲",
+						"日間部四技電子四甲"
+						
+						
+						];
+		return classess[i];
+	}
+	
+	function MONames(i){
+		var namess = [
+						"莊偉根",
+						"陳怡靜",
+						"謝佩芸",
+						"徐子棋",
+						"林廷翰",
+						"楊芳鈺",
+						"蔡芳沂",
+						"許雅媛",
+						"李修齊"
+						
+						
+					];
+		return namess[i];
+	}
+	MORanking();
+	
